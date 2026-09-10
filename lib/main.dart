fb38ef6539d967acc371dbbf98dc4e05c9a83523
KasenS1230 <kasen.sinclair@cox.net>
2026-09-10 16:07:05 -0500

    .

--- lib/main.dart
+++ lib/main.dart
@@ -105,7 +105,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: .center,
           children: [
             const Text('Kasen has pushed the button this many times:', style: TextStyle(
-              fontSize: 50.0, // Adjust this number to make it bigger
+              fontSize: 51.0, // Adjust this number to make it bigger
               fontWeight: FontWeight.bold, // Optional: makes it bold too
             ),),
             Text(
